892b93b66801de60369360ff29e9830765969410
Joshua Ibrom <joshuaibrom@gmail.com>
2023-07-12 17:56:28 -0500

    Fixed train / test matrix shapes

--- src/main.zig
+++ src/main.zig
@@ -10,11 +10,10 @@ pub fn main() !void {
     var timer = try std.time.Timer.start();
 
     const data = try loader.read_train_file(allocator);
-    // TODO: Maybe deinit after the subdivisions? Do we still need this data?
     defer data.items.deinit();
     defer data.labels.deinit();
     {
-        const time_taken = timer.read() / std.time.ns_per_s;
+        const time_taken = timer.lap() / std.time.ns_per_s;
         const data_attrs = try data.items.sizeAsStr();
         const data_labels = try data.labels.sizeAsStr();
         try stdout.print("Loaded {s} attributes with {s} labels in {d} seconds.\n", .{ data_attrs, data_labels, time_taken });
@@ -22,14 +21,21 @@ pub fn main() !void {
     }
     try stdout.print("Subdividing datasets.\n", .{});
     try bw.flush();
-    const train_items = try data.items.subdivide(10000, data.items.cols, 0, 0);
+    const train_items = try (try data.items.subdivide(10000, data.items.cols, 0, 0)).transpose();
     defer train_items.deinit();
-    const train_labels = try data.labels.subdivide(10000, 1, 0, 0);
+    const train_labels = try (try data.labels.subdivide(10000, 1, 0, 0)).transpose();
     defer train_labels.deinit();
-    const test_items = try data.items.subdivide(50000, data.items.cols, 10000, 0);
+    const test_items = try (try data.items.subdivide(50000, data.items.cols, 10000, 0)).transpose();
     defer test_items.deinit();
-    const test_labels = try data.labels.subdivide(50000, 1, 10000, 0);
+    const test_labels = try (try data.labels.subdivide(50000, 1, 10000, 0)).transpose();
     defer test_labels.deinit();
-    try stdout.print("Divided:\n  Train Items: {s}\n  Train Labels: {s}\n  Test Items: {s}\n  Tests Labels: {s}\n", .{ try train_items.sizeAsStr(), try train_labels.sizeAsStr(), try test_items.sizeAsStr(), try test_labels.sizeAsStr() });
-    try bw.flush();
+    {
+        const time_taken = timer.read() / std.time.ns_per_s;
+        const trnis = try train_items.sizeAsStr();
+        const trnls = try train_labels.sizeAsStr();
+        const tstis = try test_items.sizeAsStr();
+        const tstls = try test_labels.sizeAsStr();
+        try stdout.print("Divided (Took {d} seconds):\n  Train Items: {s}\n  Train Labels: {s}\n  Test Items: {s}\n  Tests Labels: {s}\n", .{ time_taken, trnis, trnls, tstis, tstls });
+        try bw.flush();
+    }
 }
