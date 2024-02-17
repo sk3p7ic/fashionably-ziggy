2564a621bcfd2fc3639340d0aec8d7144f8d31d9
Joshua Ibrom (sk3p7ic) <joshuaibrom@gmail.com>
2024-02-16 18:09:12 -0600

    Updated dataset prep to export data as raw bytes for easier parsing.

--- dataset/download.py
+++ dataset/download.py
@@ -1,4 +1,5 @@
-import os, csv, gzip
+import os
+import gzip
 import numpy as np
 
 MODE = 'train'
@@ -12,21 +13,15 @@ print(f'Loading files from\n  {path_labels=}\n  {path_images=}')
 # Load the data from the input files
 with gzip.open(path_labels, 'rb') as f:
     labels: np.ndarray = np.frombuffer(f.read(), dtype=np.uint8,
-                           offset=8)
+                                       offset=8)
 with gzip.open(path_images, 'rb') as f:
     images: np.ndarray = np.frombuffer(f.read(), dtype=np.uint8,
-                           offset=16).reshape(len(labels), 784)
+                                       offset=16).reshape(len(labels), 784)
 
 print(f'Loaded {images.shape=}, {labels.shape=}...')
 print("Writing to file.")
 
-# Transform the input data
-output_arr = []
-for i in range(len(labels)):
-    output_arr.append(np.insert(images[i], 0, labels[i]).tolist())
-
-with open('fashion-mnist.csv', 'w') as csvf:
-    writer = csv.writer(csvf)
-    writer.writerows(output_arr)
-print("Done.")
-
+dataset = np.insert(images, 0, labels, 1).astype('uint8')
+train_output_filename = 'fashion-mnist-train.dataset'
+dataset.tofile(train_output_filename)
+print(f'Wrote {dataset.nbytes} bytes to {train_output_filename=}')
